d4328e46f941e134b18728cd6e6ff1c3c0bca06d
Zhi Wong <zephyrprime@gmail.com>
2025-05-14 15:30:59 -0500

    ...

--- settings.php
+++ settings.php
@@ -1,10 +1,10 @@
 <?php
 ## --- SETTINGS ----------------
 
-$yourDomain = "https://yourdomain.com";
-$addressToReportTo = "mail@yourdomain.com";
-$projectDirectory = "upload/";
-$uploadDirectory = "uploadedFiles/";
+$yourDomain = "https://localhost";
+$addressToReportTo = "mail@localhost";
+$projectDirectory = '';#"upload/";
+$uploadDirectory = '';#"uploadedFiles/";
 
 # Default Path for Uploaded-Files would be: https://yourdomain.com/upload/uploadedFiles/
 ## --- SETTINGS END ----------------
